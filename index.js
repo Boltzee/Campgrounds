f734a262f2bdf3ab547f5eeefc68b52945ae1013
Kiran Tirunagiri <>
2021-03-19 22:46:14 +0530

    required the model directory into the index file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const path = require("path");
 const app = express();
+const Campground = require('./models/campground');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
