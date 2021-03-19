fda31ee91292248c5d06be10ddda2971659ffcb2
Kiran Tirunagiri <>
2021-03-19 22:44:32 +0530

    Added the model directory
    it contains all of the upcoming models schemas... it may have other stuff to it... but that is all i know till now.

---
+++ models/campground.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const campgroundSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required : true
+	}, 
+	price : {
+		type: Number, 
+		required: true
+	},
+	description : {
+		type: String,
+		lowercase: true
+	},
+	location : {
+		type: String
+	}
+})
+
+const Campground = mongoose.model('Campground', campgroundSchema);
+
+module.exports = Campground;
