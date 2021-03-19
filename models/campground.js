f768529d3a3440e6bf2ed66d1b4d0adba71eb0b8
Kiran Tirunagiri <>
2021-03-19 22:53:30 +0530

    Nothing much has been changed

--- models/campground.js
+++ models/campground.js
@@ -1,23 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const campgroundSchema = new mongoose.Schema({
-	name: {
+	title: {
 		type: String,
-		required : true
-	}, 
-	price : {
-		type: Number, 
-		required: true
+		required: true,
 	},
-	description : {
+	price: {
+		type: Number,
+		required: true,
+	},
+	description: {
 		type: String,
-		lowercase: true
+		lowercase: true,
 	},
-	location : {
-		type: String
-	}
-})
+	location: {
+		type: String,
+	},
+});
 
-const Campground = mongoose.model('Campground', campgroundSchema);
+const Campground = mongoose.model("Campground", campgroundSchema);
 
 module.exports = Campground;
