587c49296e379639c0eb4ea6e1e45b01822ab3d3
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-03-22 23:20:33 +0530

    Added validations for image,location , description

--- index.js
+++ index.js
@@ -123,12 +123,15 @@ app.post(
 			campground: Joi.object({
 				title: Joi.string().required(),
 				price: Joi.number().required().min(0),
+				image: Joi.string().required(),
+				location : Joi.string().required(),
+				description: Joi.string().required()
 			}).required(),
 		});
-		const {error} = campgroundSchema.validate(req.body);
+		const { error } = campgroundSchema.validate(req.body);
 		if (error) {
-			const msg = error.details.map(el => el.message).join(,);
-			throw new ExpressError(result.error.details, 400);
+			const msg = error.details.map((el) => el.message).join(",");
+			throw new ExpressError(msg, 400);
 		}
 		const ground = new Campground(req.body.campground); // to handle the async error
 		await ground.save();
