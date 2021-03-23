cbb364026f6223ae9a37d2360eba274d755b4448
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-03-24 03:21:52 +0530

    Added two additional routes.
    1) Route to delete a perticular review for a perticular campground.
    2) Route to edit a review of a perticular campground.
    for achieving the goal 1, I had to create a function removeByAttr to delete the perticular review from the campground.reviews
    
    NOTE: both of them use /campgrounds/:groundId/review/:reviewId path

--- index.js
+++ index.js
@@ -176,19 +176,51 @@ app.post(
 
 //
 
-// Route for deleting a perticular review of a campground 
+// Route for deleting a perticular review of a campground
 
-app.delete('/campgrounds/:groundId/review/:reviewId', catchAsync( async (req, res) => {
-	const {groundId, reviewId} = req.params;
-	const ground = await Campground.findById(groundId).populate('reviews');
-	const review = await Review.findByIdAndDelete(reviewId);
-	console.log(review);
-	res.send('successfully deleted the review');
-	// ground = ground.reviews.filter( rev => rev._id!==review._id);
-	// console.log(ground);
-	// await ground.save();
-	// res.redirect(`/campgrounds/${ground._id}`)
-}))
+var removeByAttr = function (arr, attr, value) {
+	/// function to delete an review from the reviews array
+	var i = arr.length;
+	while (i--) {
+		if (arr[i] && arr[i][attr] == value) {
+			arr.splice(i, 1);
+		}
+	}
+	return arr;
+};
+
+app.delete(
+	"/campgrounds/:groundId/review/:reviewId",
+	catchAsync(async (req, res) => {
+		const { groundId, reviewId } = req.params;
+		let ground = await Campground.findById(groundId).populate("reviews");
+		console.log(ground);
+		removeByAttr(ground.reviews, "_id", reviewId);
+		await ground.save();
+		const review = await Review.findByIdAndDelete(reviewId);
+		console.log(review);
+		res.redirect(`/campgrounds/${ground._id}`);
+	})
+);
+
+//
+
+// Route for Edit/Patch a perticular review
+
+app.patch(
+	"/campgrounds/:groundId/review/:reviewId",
+	validateReview,
+	catchAsync(async (req, res) => {
+		const { groundId, reviewId } = req.params;
+		const update = req.body.campground;
+		// console.log(update);
+		const see = await Review.findByIdAndUpdate(reviewId, update, {
+			new: true,
+		});
+		console.log(see);
+		res.redirect(`/campgrounds/${groundId}`);
+	})
+);
 
 //
 
