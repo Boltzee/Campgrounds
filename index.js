0a323d87f229b4b22ff7a49689a4759900c87b04
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-03-26 13:08:00 +0530

    Moved the validation campground middleware to its respective route.

--- index.js
+++ index.js
@@ -19,15 +19,7 @@ app.use(methodOverride("_method"));
 
 // Custom defined middleware functions
 
-const validateCampground = (req, res, err) => {
-	const { error } = campgroundSchema.validate(req.body);
-	if (error) {
-		const msg = error.details.map((el) => el.message).join(",");
-		throw new ExpressError(msg, 400);
-	} else {
-		next();
-	}
-};
+
 
 const validateReview = (req, res, next) => {
 	const { error } = reviewSchema.validate(req.body);
