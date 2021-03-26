8bbf4a3df8372ad54c879ab52944ab47d907d38b
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-03-26 13:08:42 +0530

    Created routes sub directory and created campgrounds.js file within it containing camground routes.

---
+++ routes/campgrounds.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const validateCampground = (req, res, err) => {
+	const { error } = campgroundSchema.validate(req.body);
+	if (error) {
+		const msg = error.details.map((el) => el.message).join(",");
+		throw new ExpressError(msg, 400);
+	} else {
+		next();
+	}
+};
+
+module.exports = router;
