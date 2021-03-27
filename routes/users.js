a94e03a9a2e69ee8deb13227d49d761feb1a62c7
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-03-28 00:46:01 +0530

    used userLogin.

--- routes/users.js
+++ routes/users.js
@@ -20,12 +20,7 @@ router.post(
 		failureFlash: true,
 		failureRedirect: "/login",
 	}),
-	(req, res) => {
-		req.flash("success", "Welcome back");
-		const redirectUrl = req.session.returnTo || "/campgrounds";
-		delete req.session.returnTo;
-		res.redirect(redirectUrl);
-	}
+	users.userLogin
 );
 
 router.get("/logout", (req, res) => {
