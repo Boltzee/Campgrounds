c62218aeee4adf924b9a6628fecf71e82a591035
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-03-28 00:37:27 +0530

    Implemented the createUser functionality.

--- controllers/users.js
+++ controllers/users.js
@@ -5,3 +5,19 @@ const User = require("../models/user");
 module.exports.registerForm = (req, res) => {
 	res.render("users/register");
 };
+
+module.exports.createUser = async (req, res, next) => {
+	try {
+		const { username, password, email } = req.body;
+		const user = new User({ email, username });
+		const result = await User.register(user, password);
+		req.login(result, (err) => {
+			if (err) return next(err);
+			req.flash("success", "Welcome to yelpcamp");
+			res.redirect("/campgrounds");
+		});
+	} catch (e) {
+		req.flash("error", e.message);
+		res.redirect("/register");
+	}
+};
