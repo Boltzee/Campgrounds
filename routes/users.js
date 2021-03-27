10382623d2c5fd6e9f007039e699f4a7e70fc407
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-03-27 16:17:45 +0530

    Refactored the code to look nicer.

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const passport = require('passport');
+const passport = require("passport");
 
 const User = require("../models/user");
 
@@ -10,26 +10,36 @@ router.get("/register", (req, res) => {
 	res.render("users/register");
 });
 
-router.post("/register", catchAsync(async (req, res) => {
-	try {
-		const { username, password, email } = req.body;
-		const user = new User({ email, username });
-		const result = await User.register(user, password);
-		req.flash('success', 'Welcome to yelpcamp');
-		res.redirect('/campgrounds');
-	} catch(e) {
-		req.flash('error', e.message);
-		res.redirect('/register');
+router.post(
+	"/register",
+	catchAsync(async (req, res) => {
+		try {
+			const { username, password, email } = req.body;
+			const user = new User({ email, username });
+			const result = await User.register(user, password);
+			req.flash("success", "Welcome to yelpcamp");
+			res.redirect("/campgrounds");
+		} catch (e) {
+			req.flash("error", e.message);
+			res.redirect("/register");
+		}
+	})
+);
+
+router.get("/login", (req, res) => {
+	res.render("users/login");
+});
+
+router.post(
+	"/login",
+	passport.authenticate("local", {
+		failureFlash: true,
+		failureRedirect: "/login",
+	}),
+	(req, res) => {
+		req.flash("success", "Welcome back");
+		res.redirect("/campgrounds");
 	}
-}));
-
-router.get('/login', (req, res) => {
-	res.render('users/login');
-})
-
-router.post('/login', passport.authenticate('local',{failureFlash: true, failureRedirect : '/login'}), (req, res) => {
-	req.flash('success','Welcome back');
-	res.redirect('/campgrounds');
-})
+);
 
 module.exports = router;
