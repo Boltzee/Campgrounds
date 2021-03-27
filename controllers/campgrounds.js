041a7e67a771673daa1d4a34dbabb662756e65b5
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-03-28 00:02:51 +0530

    Implemented getCampgroundById.

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -8,3 +8,20 @@ module.exports.index = async (req, res) => {
 module.exports.newCampgroundForm = (req, res) => {
 	res.render("campground/new", { cities }); // route for displaying form to create a new campground
 };
+
+module.exports.getCampgroundById = async (req, res) => {
+	const { id } = req.params;
+	const ground = await Campground.findById(id)
+		.populate({
+			path: "reviews",
+			populate: {
+				path: "author",
+			},
+		})
+		.populate("author");
+	if (!ground) {
+		req.flash("error", "Requested campground cannot be found");
+		return res.redirect("/campgrounds");
+	}
+	res.render("campground/details", { ground });
+};
