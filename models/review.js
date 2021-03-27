7f682bedcc918ba8c9e999508b7bd7f7f8fccb53
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-03-27 18:50:57 +0530

    Added the author field to the review model and also imported the User model.

--- models/review.js
+++ models/review.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
+const User = require("./user");
 const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
 	body: String,
 	rating: Number,
+	author: {
+		type: Schema.Types.objectID,
+		ref: "User",
+	},
 });
 
 module.exports = mongoose.model("Review", reviewSchema);
