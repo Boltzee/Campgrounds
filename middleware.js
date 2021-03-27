7e090adb524a0c74589882d1fe52d56e5644576e
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-03-27 18:29:18 +0530

    Added the validateCampground and isAuthor middlewares.

--- middleware.js
+++ middleware.js
@@ -6,3 +6,23 @@ module.exports.isLoggedIn = (req, res, next) => {
 	}
 	next();
 };
+
+module.exports.validateCampground = (req, res, next) => {
+	const { error } = campgroundSchema.validate(req.body);
+	if (error) {
+		const msg = error.details.map((el) => el.message).join(",");
+		throw new ExpressError(msg, 400);
+	} else {
+		next();
+	}
+};
+
+module.exports.isAuthor = async (req, res, next) => {
+	const { id } = req.params;
+	const ground = await Campground.findById(id);
+	if (!ground.author.equals(req.user._id)) {
+		req.flash("error", "You do not have the permission to do that");
+		return res.redirect(`/campgrounds/${id}`);
+	}
+	next();
+};
