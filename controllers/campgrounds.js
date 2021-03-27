c2e2a3bec171489b8ca8bb117b4d353484394a07
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-03-27 23:59:20 +0530

    Created the new campground form controller.

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -4,3 +4,7 @@ module.exports.index = async (req, res) => {
 	const grounds = await Campground.find({});
 	res.render("campground/show", { grounds });
 };
+
+module.exports.newCampgroundForm = (req, res) => {
+	res.render("campground/new", { cities }); // route for displaying form to create a new campground
+};
