b8dd03ffac983817a7ea0dd5af68c6df354901ff
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-03-27 14:42:46 +0530

    Corrected the route.

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 
-router.get('register', (req, res) => {
-	res.render('users/register');
-})
+router.get("/register", (req, res) => {
+	res.render("users/register");
+});
 
 module.exports = router;
-
