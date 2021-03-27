d7f2440c2a8048e7370168fc9e3a6ed2c1020185
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-03-28 00:24:58 +0530

    Moved the create review for campground functionality to its controller.
    Also required the review controller.

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,6 +4,8 @@ const router = express.Router({ mergeParams: true });
 const Campground = require("../models/campground");
 const Review = require("../models/review");
 
+const reviews = require('../controllers/reviews');
+
 const catchAsync = require("../utils/catchAsync");
 
 const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
@@ -13,24 +15,7 @@ router.post(
 	"/",
 	isLoggedIn,
 	validateReview,
-	catchAsync(async (req, res) => {
-		const { id } = req.params;
-		const campground = await Campground.findById(id);
-		if (!campground) {
-			req.flash("error", "Requested campground cannot be found");
-			return res.redirect("/campgrounds");
-		}
-		const { review } = req.body;
-		const rev = new Review(review);
-		rev.author = req.user._id;
-		campground.reviews.push(rev);
-		await campground.save();
-		await rev.save();
-		req.flash("success", "Successfully created a review");
-		res.redirect(`/campgrounds/${campground._id}`);
-		// console.log(review);
-		// res.send(review);
-	})
+	catchAsync(reviews.createReviewForCampground)
 );
 
 //
