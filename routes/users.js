c8304b357d67a195df671e631669986a73ce60d1
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-03-27 15:30:27 +0530

    Imported catchAsync in the users route and used it.
    Also wrote a try and catch block if there is any error within the registration process... Like username has already been taken.

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const router = express.Router();
+
 const User = require("../models/user");
+
+const catchAsync = require("../utils/catchAsync");
 
 router.get("/register", (req, res) => {
 	res.render("users/register");
 });
 
-router.post("/register", async (req, res) => {
-	console.log(req.body);
-	const { username, password, email } = req.body;
-	const user = new User({ email, username });
-	const result = await User.register(user, password);
-	// res.send(result);
-	// res.send("Successfully added you... Welcome to the family!!");
-	req.flash('success', 'Welcome to yelpcamp');
-	res.redirect('/campgrounds');
-});
+router.post("/register", catchAsync(async (req, res) => {
+	try {
+		const { username, password, email } = req.body;
+		const user = new User({ email, username });
+		const result = await User.register(user, password);
+		req.flash('success', 'Welcome to yelpcamp');
+		res.redirect('/campgrounds');
+	} catch(e) {
+		req.flash('error', e.message);
+		res.redirect('/register');
+	}
+}));
 
 module.exports = router;
