4e0d107ec05641a663364b9ca1a5bd5088609c02
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-03-27 23:55:54 +0530

    Imported the campground model.

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,3 +1,5 @@
+const Campground = require("../models/campground");
+
 module.exports.index = async (req, res) => {
 	const grounds = await Campground.find({});
 	res.render("campground/show", { grounds });
