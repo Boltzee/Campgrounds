49508f36f6216a3fd5c402612f89146dea883d1a
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-03-29 02:40:09 +0530

    Included all the mapbax dependencies.

--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 const Campground = require("../models/campground");
 const cities = require("./cities");
 const { descriptors, places } = require("./seedHelpers");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapboxToken = process.env.MAPBOX_TOKEN;
+
+const geocoder = mbxGeocoding({ accessToken: mapboxToken });
 
 mongoose.connect("mongodb://localhost:27017/YelpCamp", {
 	useNewUrlParser: true,
