ea484b566c42c03244005fcb6322ee3d23215a0e
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-03-29 03:04:20 +0530

    map style changed to navigation-guidance-night-v4

--- public/js/showpageMap.js
+++ public/js/showpageMap.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = mapToken;
 var map = new mapboxgl.Map({
 	container: "map", // container ID
-	style: "mapbox://styles/mapbox/dark-v10", // style URL
+	style: "mapbox://styles/mapbox/navigation-guidance-night-v4", // style URL
 	center: campground.geometry.coordinates, // starting position [lng, lat]
 	zoom: 9, // starting zoom
 });
