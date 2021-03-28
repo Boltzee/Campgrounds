6467a04e0a435b310acaacc0f80a14d1e0dc4143
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-03-29 03:08:03 +0530

    Nothing much changed

--- public/js/showpageMap.js
+++ public/js/showpageMap.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = mapToken;
 var map = new mapboxgl.Map({
 	container: "map", // container ID
-	style: "mapbox://styles/mapbox/navigation-guidance-night-v4", // style URL
+	style: "mapbox://styles/mapbox/navigation-preview-night-v4", // style URL
 	center: campground.geometry.coordinates, // starting position [lng, lat]
 	zoom: 9, // starting zoom
 });
