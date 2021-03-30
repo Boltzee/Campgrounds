28cb40a8b4b7c397ef50e9613abeeb42848da7c1
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-03-30 14:49:57 +0530

    Configured the dbUrl

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(mongoSanitize());
 
 // const db_url = process.env.DB_URL;
 // "mongodb://localhost:27017/YelpCamp"
-const dbUrl = "mongodb://localhost:27017/YelpCamp";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/YelpCamp";
 mongoose.connect(dbUrl, {
 	useNewUrlParser: true,
 	useCreateIndex: true,
