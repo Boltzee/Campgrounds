adabc57f0893a25d9831b03d2734b16c7fe43d8e
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-03-30 13:41:06 +0530

    Connected to the cloud database.

--- index.js
+++ index.js
@@ -133,7 +133,9 @@ app.set("views", path.join(__dirname, "views"));
 
 /// Connecting to the mongo database
 
-mongoose.connect("mongodb://localhost:27017/YelpCamp", {
+const db_url = process.env.DB_URL;
+// "mongodb://localhost:27017/YelpCamp"
+mongoose.connect(db_url, {
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useUnifiedTopology: true,
