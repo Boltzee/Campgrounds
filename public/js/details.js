5d806eeb64be907abf0fcb46e03195e94f81457f
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-04-12 20:04:08 +0530

    Addtional functionality for the new details page.

--- public/js/details.js
+++ public/js/details.js
@@ -1,7 +1,9 @@
 const carousel = document.querySelector("#campgroundCarousel");
 const detailCard = document.querySelector("#detailCard");
+const contentCol = document.querySelector("#contentCol");
 
 let detail_card_style = 0;
+let detail_card_style2 = 0;
 
 window.addEventListener("DOMContentLoaded", function (e) {
 	if (window.innerWidth <= 1012) {
@@ -10,6 +12,11 @@ window.addEventListener("DOMContentLoaded", function (e) {
 		detail_card_style = 0;
 	} else {
 		detail_card_style = 1;
+	}
+	///////////////////////////////////
+	if (window.innerWidth <= 594) {
+		contentCol.classList.remove("col-9");
+		detail_card_style2 = 0;
 	}
 });
 
@@ -22,5 +29,13 @@ window.addEventListener("resize", function (e) {
 		carousel.classList.add("col-4");
 		detailCard.classList.add("col-8");
 		detail_card_style = 1;
+	}
+	///////////////////////////////
+	if (window.innerWidth <= 594 && detail_card_style2 != 0) {
+		contentCol.classList.remove("col-9");
+		detail_card_style2 = 0;
+	} else if (window.innerWidth > 594 && detail_card_style2 != 1) {
+		contentCol.classList.add("col-9");
+		detail_card_style2 = 1;
 	}
 });
