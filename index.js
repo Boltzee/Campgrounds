e6c1f53f04366d6615547d0b9c3d5e01b2623ad6
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-04-16 13:24:55 +0530

    Back to the dev mode for the final home page.

--- index.js
+++ index.js
@@ -38,8 +38,8 @@ app.use(mongoSanitize());
 
 // const db_url = process.env.DB_URL;
 // "mongodb://localhost:27017/YelpCamp"
-const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/YelpCamp";
-// const dbUrl = "mongodb://localhost:27017/YelpCamp";
+// const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/YelpCamp";
+const dbUrl = "mongodb://localhost:27017/YelpCamp";
 mongoose.connect(dbUrl, {
 	useNewUrlParser: true,
 	useCreateIndex: true,
